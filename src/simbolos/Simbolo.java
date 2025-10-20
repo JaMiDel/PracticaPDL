1a360e941ce9ab7dd7db258b82966e82878336ab
JaMiDel <jdmp862005@gmail.com>
2025-10-20 22:22:37 +0200

    Tabla de simbolos

--- src/simbolos/Simbolo.java
+++ src/simbolos/Simbolo.java
@@ -2,8 +2,8 @@ package simbolos;
 
 public class Simbolo {
     public final String nombre;
-    public final String id;
-    public Simbolo(String nombre, String id) {
+    public final int id;
+    public Simbolo(String nombre, int id) {
         this.nombre = nombre;
         this.id = id;
     }
--- src/simbolos/TablaSimbolos.java
+++ src/simbolos/TablaSimbolos.java
@@ -1,4 +1,26 @@
 package simbolos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TablaSimbolos {
+    private final Map<String, Simbolo> table;
+    private int nextId;
+
+    public TablaSimbolos() {
+        table = new HashMap<>();
+        nextId = 0;
+    }
+
+    public Simbolo findOrInsert(String lexema) {
+        if (table.containsKey(lexema)) {
+            return table.get(lexema);
+        } else {
+            Simbolo nuevoSimbolo = new Simbolo(lexema, nextId);
+            table.put(lexema, nuevoSimbolo);
+            nextId++;
+
+            return nuevoSimbolo;
+        }
+    }
 }
